f1852f430ef44bf987997075b2f7a6041a9090f5
GaryNotGay <44407710+GaryNotGay@users.noreply.github.com>
2020-02-20 16:42:06 +0800

    Add Default Address

--- Android_Photo.py
+++ Android_Photo.py
@@ -3,8 +3,8 @@
 # @Author  :  lijishi
 # @Contact :  lijishi@emails.bjut.edu.cn
 # @Software:  Pycharm & Python 3.7
-# @EditTime:  Jan 8, 2020
-# @Version :  1.0
+# @EditTime:  Feb 20, 2020
+# @Version :  1.1
 # @describe:  GUI of Control MIUI Photoed
 # @LICENSE :  GNU GENERAL PUBLIC LICENSE Version 3
 
@@ -24,7 +24,7 @@ from tkinter import *
 from picture import Icon
 from picture import Gif
 
-num = -1
+num = 0
 def Photo():
     num_limit = int(photo_num.get())
     time_gap = int(photo_time.get())
@@ -35,15 +35,15 @@ def Photo():
     last_num.set(num_limit-num)
     last_time.set((num_limit-num) * time_gap)
 
-    start_camera = path + r'\adb shell am start -a android.media.action.STILL_IMAGE_CAMERA'
+#    start_camera = path + r'\adb shell am start -a android.media.action.STILL_IMAGE_CAMERA'
     #os.system(start_camera)
-    subprocess.run(start_camera, shell=True, stdin=subprocess.PIPE, stdout=subprocess.PIPE, stderr=subprocess.PIPE)
+#    subprocess.run(start_camera, shell=True, stdin=subprocess.PIPE, stdout=subprocess.PIPE, stderr=subprocess.PIPE)
     Shutter(path)
     if num == num_limit:
         have_num.set(num)
         last_num.set(num_limit - num)
         last_time.set((num_limit - num) * time_gap)
-        num = -1
+        num = 0
         Finish(path)
     else:
         main_window.after(time_gap * 1000, Photo)
@@ -61,7 +61,7 @@ def SelectADBPath():
     path_ = tkinter.filedialog.askopenfilename()
 #    path_ = path_.replace("/", "\\\\")
     adb_path.set(path_[:-7])
-    shutter_path = path_[:-7]
+#    shutter_path = path_[:-7]
 
 def About():
     # window centered
@@ -93,9 +93,9 @@ def About():
     software_frame.grid(row=0, column=0, rowspan=5, columnspan=4, padx=50, pady=5)
     ttk.Label(software_frame, image=cha_gif, compound='left').grid(row=0, rowspan=3, column=0)
     os.remove('tmp.gif')
-    ttk.Label(software_frame, text="Android Photo Version 1.0").grid(row=0, column=1, sticky = W)
+    ttk.Label(software_frame, text="Android Photo Version 1.1").grid(row=0, column=1, sticky = W)
     ttk.Label(software_frame, text="@Author    :   lijishi").grid(row=1, column=1, sticky = W)
-    ttk.Label(software_frame, text="@EditTime  :   Jan 8,2020").grid(row=2, column=1, sticky = W)
+    ttk.Label(software_frame, text="@EditTime  :   Feb 20,2020").grid(row=2, column=1, sticky = W)
 
     copyright_frame = ttk.LabelFrame(about_window, text='LICENSE Info')
     copyright_frame.grid(row=5, column=0, rowspan=3, columnspan=4, padx=50, pady=5)
@@ -103,13 +103,13 @@ def About():
     ttk.Label(copyright_frame, text="GNU GENERAL PUBLIC LICENSE Version 3").grid(row=6, column=0)
 
 def Finish(path):
-    back_command = path + r'\adb shell input keyevent 4'
+#    back_command = path + r'\adb shell input keyevent 4'
     #os.system(back_command)
-    subprocess.run(back_command, shell=True, stdin=subprocess.PIPE, stdout=subprocess.PIPE, stderr=subprocess.PIPE)
+#    subprocess.run(back_command, shell=True, stdin=subprocess.PIPE, stdout=subprocess.PIPE, stderr=subprocess.PIPE)
     tk.messagebox.showinfo("Finish", "拍照完成！")
 
 def Tips():
-    tk.messagebox.showinfo("Tips", "确保手机开启USB调试\nadb工具包请自行下载\n间隔时间仅支持整数\n确保初始位置为手机桌面")
+    tk.messagebox.showinfo("Tips", "确保手机开启USB调试\n间隔时间仅支持整数\n确保初始位置为拍照界面\nadb工具包请自行下载\n若默认adb路径错误可手动更改")
 
 def Error():
     tk.messagebox.showerror("Error", "错误")
@@ -125,7 +125,7 @@ y = (screen_heigh-main_window_heigh) / 2
 main_window.geometry("%dx%d+%d+%d" %(main_window_width,main_window_heigh,x,y))
 
 # window layout
-main_window.title("Android Photo V1.0")
+main_window.title("Android Photo V1.1")
 with open('tmp.ico', 'wb') as tmp:
     tmp.write(base64.b64decode(Icon().img))
 main_window.iconbitmap('tmp.ico')
@@ -138,7 +138,11 @@ last_time = tk.IntVar()
 last_num = tk.IntVar()
 have_num = tk.IntVar()
 output = tk.IntVar()
-adb_path.set("请选择/键入adb.exe文件位置")
+#adb_path.set("请选择/键入adb.exe文件位置")
+file_addr = os.getcwd()
+addr = file_addr + r"\adb"
+addr = addr.replace("\\", "/")
+adb_path.set(addr)
 last_time.set('0')
 last_num.set('0')
 have_num.set('0')
